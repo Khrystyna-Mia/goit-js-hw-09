c799f999baf6aea53d478d38c4b15b594b638193
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-02-01 21:16:52 +0200

    01.02

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+'use strict';
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    // console.log(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
