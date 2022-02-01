75854691cc47e3d56c7708bd9d6127e85a7df0de
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-02-01 23:53:40 +0200

    01.02

---
+++ src/js/02-timer.js
@@ -0,0 +1,43 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputEl = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const resetBtn = document.querySelector('[data-reset]');
+const dataDays = document.querySelector('[data-days]');
+const dataH = document.querySelector('[data-hours]');
+const dataM = document.querySelector('[data-minutes]');
+const dataS = document.querySelector('[data-seconds]');
+
+// startBtn.disabled = true;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
