e7f51cfcc3c36acce532b8a1f8cc06d7d318d122
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-02-06 20:35:14 +0200

    06.02

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', promiseSubmit);
+
+function promiseSubmit(evt) {
+    evt.preventDefault();
+
+    let [delay, step, amount] = evt.currentTarget.elements;
+    delay = Number(delay.value);
+    step = Number(step.value);
+    amount = Number(amount.value);
+
+    for (let position = 1; position <= amount; position ++) {
+        if (position < 1) {
+            delay = + step;
+        }
+
+        createPromise(position, delay)
+            .then(({ position, delay }) => {
+                setTimeout(() => {
+                    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+                }, delay);
+            })
+            .catch(({ position, delay }) => {
+                setTimeout(() => {
+                    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+                }, delay);
+            });
+    }
+}
+ 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    return new Promise((resolve, reject) => {
+        const shouldResolve = Math.random() > 0.3;
+        
+        if (shouldResolve) {
+            // Fulfill
+            resolve({ position, delay });
+        } else {
+            // Reject
+            reject({ position, delay });
+        }
+    });
 }
