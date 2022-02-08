93035decaa8c72b62fd79ba1419ea603ed52f359
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-02-09 01:56:53 +0200

    09.02

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+// import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl = document.querySelector('.form');
@@ -13,34 +14,42 @@ function promiseSubmit(evt) {
     amount = Number(amount.value);
 
     for (let position = 1; position <= amount; position ++) {
-        if (position < 1) {
-            delay = + step;
-        }
+        // if (position < 1) {
+        //     delay += step;
+        // }
+        
 
-        createPromise(position, delay)
-            .then(({ position, delay }) => {
-                setTimeout(() => {
-                    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-                }, delay);
-            })
-            .catch(({ position, delay }) => {
-                setTimeout(() => {
-                    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-                }, delay);
-            });
+        createPromise({ position, delay })
+            .then(({ position, delay }) => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+            .catch(({position, delay}) => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+         
+        // createPromise(position, delay)
+        //     .then(({ position, delay }) => {
+        //         setTimeout(() => {
+        //             Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        //         }, delay);
+        //     })
+        //     .catch(({ position, delay }) => {
+        //         setTimeout(() => {
+        //             Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        //         }, delay);
+        //     });
+        delay += step;
     }
 }
  
 function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+
     return new Promise((resolve, reject) => {
-        const shouldResolve = Math.random() > 0.3;
-        
-        if (shouldResolve) {
-            // Fulfill
-            resolve({ position, delay });
-        } else {
-            // Reject
-            reject({ position, delay });
-        }
+        setTimeout(() => {
+            if (shouldResolve) {
+                // Fulfill
+                resolve({ position, delay });
+            } else {
+                // Reject
+                reject({ position, delay });
+            }
+        }, delay);
     });
 }
