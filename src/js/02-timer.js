54530d99a363c69c7014f484fb79a1fd31214e56
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-02-10 01:09:18 +0200

    10.02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,15 +9,13 @@ const dataHours = document.querySelector('[data-hours]');
 const dataMinutes = document.querySelector('[data-minutes]');
 const dataSeconds = document.querySelector('[data-seconds]');
 
-let selectedDate = null;
 let intervalId = null;
+let selectDate = 0;
 
 startBtn.disabled = true;
-
 startBtn.addEventListener('click', start);
 stopBtn.addEventListener('click', stop);
 
-/* Вторым аргументом функции flatpickr(selector, options) передаем необязательный объект параметров. */
 
 const options = {
   enableTime: true,
@@ -26,64 +24,25 @@ const options = {
   minuteIncrement: 1,
 
   /* В методе onClose() обрабатываем дату выбранную пользователем: 
-  если пользователь не выбрал дату в будущем - failure, если верная дата - success*/
+/   если пользователь не выбрал дату в будущем - failure, если верная дата - success*/
 
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() < Date.now()) {
+    if (selectedDates[0].getTime() < options.defaultDate) {
       return Notify.failure('Please choose a date in the future');
 
     } else {
       Notify.success('The selected date is valid!');
-
-      startBtn.disabled = false;
-      selectedDate = selectedDates[0].getTime();
     }
-  },
-};
 
-const calendar = flatpickr('#datetime-picker', options);
+    startBtn.disabled = false;
+    selectDate = selectedDates[0].getTime();
 
-/* Запускаем/останавливаем таймер */
-
-function start() {
-  const startTime = selectedDate;
-
-  intervalId = setInterval(() => {
-    const currentTime = Date.now();
-    const deltaTime = startTime - currentTime;
-
+    let deltaTime = selectDate - Date.now();
     const { days, hours, minutes, seconds } = convertMs(deltaTime);
 
-    if (deltaTime < 0) {
-      clearInterval(intervalId);
-    }
-
-    startBtn.disabled = true;
-
     updateTimer({ days, hours, minutes, seconds });
-  }, 1000);
-}
-
-/* Останавливаем/очищаем таймер */
-
-function stop() {
-  startBtn.disabled = true;
-
-  clearInterval(intervalId);
-
-  calendar.setDate(Date.now());
-
-  dataDays.textContent = '00';
-  dataHours.textContent = '00';
-  dataMinutes.textContent = '00';
-  dataSeconds.textContent = '00';
-
-  // const time = convertMs(0);
-  // updateTimer(time);
-
-}
-
-/* Выбираем время */
+  },
+};
 
 function updateTimer({ days, hours, minutes, seconds }) {
     dataDays.textContent = `${ days }`;
@@ -92,14 +51,36 @@ function updateTimer({ days, hours, minutes, seconds }) {
     dataSeconds.textContent = `${ seconds }`;
 }
 
-/* Перед отрисовкой интефрейса форматируем значение. */
+function start() {
+  let deltaTime = 0;
+
+  intervalId = setInterval(() => {
+    deltaTime = selectDate - Date.now();
+
+    if (deltaTime <= 0) {
+      const { days, hours, minutes, seconds } = convertMs(deltaTime);
+      updateTimer({ days, hours, minutes, seconds });
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+function stop() {
+  const time = convertMs(0);
+  clearInterval(intervalId);
+
+  updateTimer(time);
+  calendar.setDate(Date.now());
+
+  startBtn.disabled = true;
+}
+
+const calendar = flatpickr('#datetime-picker', options);
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
-/* Для подсчета значений используем функцию convertMs, где ms - разница между конечной и текущей датой в миллисекундах. */
-/* Функция convertMs() возвращает объект с рассчитанным оставшимся временем до конечной даты. */
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -122,3 +103,112 @@ function convertMs(ms) {
 
 
 
+// let selectedDate = null;
+// let intervalId = null;
+// startBtn.disabled = true;
+
+// startBtn.addEventListener('click', start);
+// stopBtn.addEventListener('click', stop);
+
+// /* Вторым аргументом функции flatpickr(selector, options) передаем необязательный объект параметров. */
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+
+//   /* В методе onClose() обрабатываем дату выбранную пользователем: 
+//   если пользователь не выбрал дату в будущем - failure, если верная дата - success*/
+
+//   onClose(selectedDates) {
+//     if (selectedDates[0].getTime() < Date.now()) {
+//       return Notify.failure('Please choose a date in the future');
+
+//     } else {
+//       Notify.success('The selected date is valid!');
+
+//       startBtn.disabled = false;
+//       selectedDate = selectedDates[0].getTime();
+//     }
+//   },
+// };
+
+// const calendar = flatpickr('#datetime-picker', options);
+
+// /* Запускаем/останавливаем таймер */
+
+// function start() {
+//   const startTime = selectedDate;
+
+//   intervalId = setInterval(() => {
+//     const currentTime = Date.now();
+//     const deltaTime = startTime - currentTime;
+
+//     const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+//     if (deltaTime < 0) {
+//       clearInterval(intervalId);
+//     }
+
+//     startBtn.disabled = true;
+
+//     updateTimer({ days, hours, minutes, seconds });
+//   }, 1000);
+// }
+
+// /* Останавливаем/очищаем таймер */
+
+// function stop() {
+//   startBtn.disabled = true;
+
+//   clearInterval(intervalId);
+  
+//   // calendar.setDate(Date.now());
+
+//   // dataDays.textContent = '00';
+//   // dataHours.textContent = '00';
+//   // dataMinutes.textContent = '00';
+//   // dataSeconds.textContent = '00';
+
+//   // // const time = convertMs(0);
+//   // // updateTimer(time);
+
+// }
+
+// /* Выбираем время */
+
+// function updateTimer({ days, hours, minutes, seconds }) {
+//     dataDays.textContent = `${ days }`;
+//     dataHours.textContent = `${ hours }`;
+//     dataMinutes.textContent = `${ minutes }`;
+//     dataSeconds.textContent = `${ seconds }`;
+// }
+
+// /* Перед отрисовкой интефрейса форматируем значение. */
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+// /* Для подсчета значений используем функцию convertMs, где ms - разница между конечной и текущей датой в миллисекундах. */
+// /* Функция convertMs() возвращает объект с рассчитанным оставшимся временем до конечной даты. */
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   // Remaining hours
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   // Remaining minutes
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   // Remaining seconds
+//   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+//   return { days, hours, minutes, seconds };
+// }
